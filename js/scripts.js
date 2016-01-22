572491a454efaefab3516e575fea642fbae456af
David Remington <david.ian.remington@gmail.com>
2016-01-21 16:56:03 -0800

    add semi-functional jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
-
 var myCount = function (stop, step) {
   var myCountArr = [];
+  var newHTML = [];
   var start = 0;
   while (start < stop) {
     start+= step;
     myCountArr.push(start)
   };
 
-  return myCountArr;
+
+  for (var i = 0; i < myCountArr.length; i++) {
+    newHTML.push('<span>' + myCountArr[i] + '</span>')
+  };
 
 };
+
+$(function() {
+  $("form#counting").submit(function(event) {
+    var stopNumber = $("input[name=stop_number]").val();
+    var stepNumber = $("input[name=multiple_number]").val();
+    myCount(stopNumber, stepNumber);
+    $("p#results").empty().html(newHTML.join(""));
+  });
+
+  event.preventDefault();
+
+});
